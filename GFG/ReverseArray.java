4f33a7fc0499a56e38e638f54923a8417d055d9b
unknown <MohammadWaseem6>
2024-06-08 07:59:20 +0530

    added

--- GFG/ReverseArray.java
+++ GFG/ReverseArray.java
@@ -1,20 +1,28 @@
 public class ReverseArray {
 
-    public static int revArray(int arr[]){
-        for (int i = 0; i < arr.length/2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[arr.length-1-i];
-            arr[arr.length-1-i] = temp;
+    public static void revArray(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        // Swap the elements until the start index is less than the end index
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            // Move the indices towards the center
+            start++;
+            end--;
         }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 2, 1 };
+        revArray(arr);
+        
+        // Print the reversed array
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
-        return 0;
     }
-    //reverse array
-    public static void main(String[] args) {
-        int arr []= {5,4,3,2,1};
-        revArray(arr);
-    }
-
 }
