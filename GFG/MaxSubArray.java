fed49493171570bb923d38e2742e18a3379929aa
unknown <MohammadWaseem6>
2024-06-10 08:43:28 +0530

    MaxSubarray

---
+++ GFG/MaxSubArray.java
@@ -0,0 +1,23 @@
+public class MaxSubArray {
+    public static void Printmaxsubarray(int[] array) {
+        int currsum = 0;
+        int maxsum = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            currsum = currsum + array[i];
+            if (currsum > maxsum) {
+                maxsum = currsum;
+
+            }
+            if (currsum < 0) {
+                currsum = 0;
+            }
+        }
+        System.out.println(maxsum);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, -2, 6, -1, 3 };
+        Printmaxsubarray(arr);
+    }
+
+}
