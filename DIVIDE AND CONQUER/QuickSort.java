88d7b3fe1e8b3bf0fc71667e7aeef4eb566e7bc8
MohammadWaseem6 <wa6865688@gmail.com>
2024-07-08 10:00:20 +0530

    quick sort

--- DIVIDE AND CONQUER/QuickSort.java
+++ DIVIDE AND CONQUER/QuickSort.java
@@ -1,13 +1,44 @@
 public class QuickSort {
 
-    public static void quickSort(int arr[]) {
-        for(int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
-    public static void main(String[] args) {
-        int arr[]={5,3,2,7,6,1};
-        quickSort(arr);
+
+    public static void quickSort(int arr[], int si, int ei) {
+        if (si < ei) {
+            int pIndex = partition(arr, si, ei);
+            quickSort(arr, si, pIndex - 1);
+            quickSort(arr, pIndex + 1, ei);
+        }
     }
-    
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1;
+
+        for (int j = si; j < ei; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        // Swap arr[i] and pivot (arr[ei])
+        int temp = arr[i];
+        arr[i] = arr[ei];
+        arr[ei] = temp;
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 3, 2, 7, 6, 1 };
+        quickSort(arr, 0, arr.length - 1);
+        printArray(arr);
+    }
 }
